fa0287ff79bb1b44edebf27651aa3e5151e1734b
Viraj Kishore <viraj.kishore.ch@gmail.com>
2023-03-14 20:35:19 +0530

    Search bar finished. Both direct search and substring search.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -30,6 +30,21 @@ def name(request, var):
         }) 
     
 def search(request):
-    var = str(request.POST.get("q"))
-    return HttpResponseRedirect(reverse("name", kwargs={'var':var}))
+    all = util.list_entries()
+    var = str(request.POST.get("q")) # the input we got from the form 
+    exist = False
+    # if the input is in the list of titles/entries, redirect to the wiki/<title> page
+    if var in all:
+        return HttpResponseRedirect(reverse("name", kwargs={'var':var}))
     
+    # to return all pages which has a sub string of given input
+    search = []
+    for one in all:
+        if var in one:
+            search += [one]
+            exist = True
+    if exist:
+        return render(request, "encyclopedia/index.html", {
+        "entries": search
+    })
+    return HttpResponseRedirect(reverse("name", kwargs={'var':var}))
